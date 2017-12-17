8b82db19335ea91a0b73e64fd429523eccf2f270
Artemiy Luzyanin <artemluzyanin@gmail.com>
2017-12-17 15:10:52 +0300

    fix bot.py and test_image_processing.py according to changes image_processing.py

--- tests/test_image_processing.py
+++ tests/test_image_processing.py
@@ -3,25 +3,26 @@ import io
 import numpy as np
 import cv2
 
-from utils.image_processing import ndarray2bytes, convert2rgb,\
-    find_faces_n_get_labels
+from utils.image_processing import Model
+
+MODEL = Model()
 
 
 def test_ndarray2bytes():
     shape = (20, 20, 3)
     arr = np.ones(shape) * 255
     arr = arr.astype('uint8')
-    assert isinstance(ndarray2bytes(arr), io.BytesIO)
+    assert isinstance(MODEL.ndarray2bytes(arr), io.BytesIO)
 
 
 def test_convert2rgb():
     shape = (20, 20, 3)
     img = np.ones(shape) * 255
     img = img.astype('uint8')
-    assert convert2rgb(img).shape == img.shape
+    assert MODEL.convert2rgb(img).shape == img.shape
 
 
-def test_find_faces_n_get_labels():
+def test_predict_labels():
     test_img = cv2.imread("tests/test_image.jpg")
-    num_faces, output_img = find_faces_n_get_labels(ndarray2bytes(test_img))
+    num_faces, output_img = MODEL.predict_labels(MODEL.ndarray2bytes(test_img))
     assert num_faces >= 0 and isinstance(output_img, io.BytesIO)
