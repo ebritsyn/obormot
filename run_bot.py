3e582c17a0675f8d71301ea769257428d958c2cd
Sergey Rassolov <rassolov@phystech.edu>
2017-12-19 15:05:02 +0300

    fix

--- run_bot.py
+++ run_bot.py
@@ -1,14 +1,16 @@
 import os
 from bot.bot import Bot
 
+
 def main():
     token = os.environ.get('TOKEN', None)
-    db = os.environ.get('DB', 'data.db')
+    dbase = os.environ.get('DB', 'data.db')
     if token is None:
         print('Please set TOKEN as environment variable')
     else:
-        obormot = Bot(token, db)
+        obormot = Bot(token, dbase)
         obormot.start()
+
 
 if __name__ == '__main__':
     main()
